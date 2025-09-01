bf3deab1e6ecb0c961b79fc04ebac1af4963923a
Vladislav Nefedov <clay.zenx@gmail.com>
2025-09-01 11:05:45 +0300

    relative paths

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { TelegramAuthGuard } from './guard/telegram.guard';
-import { User } from 'src/common/decorators/user.decorator';
+import { User } from '../common/decorators/user.decorator';
 import { TelegramUserDto } from './dto/telegram-user.dto';
 
 @Controller('auth')
