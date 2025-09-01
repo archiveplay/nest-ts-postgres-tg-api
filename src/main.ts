cf9859ad2596884f46daec9a93c5c4dd3e0350f7
Vladislav Nefedov <clay.zenx@gmail.com>
2025-09-01 16:00:39 +0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: ['https://archiveplay.github.io'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
