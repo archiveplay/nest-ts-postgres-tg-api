fb6c92d899ae0ef4243dee4753bcc044afad92bb
Vladislav Nefedov <clay.zenx@gmail.com>
2025-09-07 16:32:29 +0300

    small log fix

--- src/user-payment/user-payment.controller.ts
+++ src/user-payment/user-payment.controller.ts
@@ -27,7 +27,7 @@ export class UserPaymentController {
     @Body() dto: CreateInvoiceDto,
     @User() user: JwtUserDto
   ) {
-    const invoicePayload = `[user-payment/top-up]: ${dto.title}_${Date.now().toLocaleString()}`;
+    const invoicePayload = `[user-payment/top-up]: ${dto.title} ${user.userId} ${new Date().toDateString()}`;
 
     await this.userPaymentService.createUserPayment(
       user.userId,
